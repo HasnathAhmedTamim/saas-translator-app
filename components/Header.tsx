78891f324b5af10204ac00a692c23d98ea9ee447
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-04-09 02:13:35 +0600

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from './Logo'
+import DarkModeToggle from './ui/DarkModeToggle'
+import UserButton from './ui/UserButton'
+
+
+function Header() {
+    return (
+        <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
+            <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto'>
+                <Logo></Logo>
+                <div className='flex-1 flex items-center justify-end space-x-4'>
+                    {/* language select */}
+
+                    {/* session && (
+
+                    ) */}
+
+                    {/* darkMode toggle */}
+                    <DarkModeToggle></DarkModeToggle>
+
+                    {/* UserButton */}
+                    <UserButton></UserButton>
+
+                </div>
+
+            </nav>
+            {/* upgrade banner */}
+        </header>
+    )
+}
+
+export default Header
---
+++ components/ui/UserButton.tsx
@@ -0,0 +1,36 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import UserAvatar from "./UserAvatar"
+
+
+
+function UserButton() {
+
+    // session...
+
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <UserAvatar name="Hasnath Abir" image="https://github.com/shadcn.png"></UserAvatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+    )
+}
+
+export default UserButton
